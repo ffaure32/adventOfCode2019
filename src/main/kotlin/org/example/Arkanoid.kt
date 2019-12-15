6235ad71e30c3eeba085d91ba30875dbbdab0c5a
ffaure32 <ffaure32@yahoo.fr>
2019-12-15 11:52:07 +0100

    leger refacto

--- src/main/kotlin/org/example/Arkanoid.kt
+++ src/main/kotlin/org/example/Arkanoid.kt
@@ -1,21 +1,9 @@
 package org.example
 
-import java.awt.EventQueue
-import java.awt.event.KeyAdapter
-import java.awt.event.KeyEvent
 import java.util.*
-import javax.swing.JFrame
 
-fun main(args: Array<String>) {
-    val input = "/day13part2.txt".loadFromFile()
-    val winnerMoves = "/winnerMoves".loadFromFile()
-    val inputJoystick = winnerMoves.lines()[0].trim().split(",").map { it.toLong() }
-    val longQueue = initLongQueue()
-    longQueue.addAll(inputJoystick)
-    println("SCORE FINAL:" + calculateDay13(input, longQueue))
-}
 
-private fun calculateDay13(input: String, queue: Queue<Long>): Long {
+public fun playArkanoid(input: String, queue: Queue<Long>): BigIntCodeComputer {
     val inputSplit = stringToLongList(input)
 
     val intCodeComputer = BigIntCodeComputer(inputSplit, queue)
@@ -23,7 +11,7 @@ private fun calculateDay13(input: String, queue: Queue<Long>): Long {
    do {
         exit = intCodeComputer.applyInstructionAtPosition()
     } while (!exit)
-    return intCodeComputer.screen.currentScore
+    return intCodeComputer
 }
 
 fun stringToLongList(input: String) = input.trim().split(",").map { it.toLong() }.toMutableList()
--- src/test/kotlin/org/example/AdventOfCodeDay13Test.kt
+++ src/test/kotlin/org/example/AdventOfCodeDay13Test.kt
@@ -2,38 +2,25 @@ package org.example
 
 import org.junit.Test
 import org.junit.jupiter.api.TestInstance
-import java.awt.event.KeyAdapter
-import java.awt.event.KeyEvent
-import java.util.*
-import javax.swing.JFrame
-import javax.swing.SwingUtilities
 import kotlin.test.assertEquals
 
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
 class AdventOfCodeDay13Test {
 
-    private fun calculateDay13(input: String, queue : Queue<Long>): Int {
-        val inputSplit = stringToLongList(input)
-
-        val intCodeComputer = BigIntCodeComputer(inputSplit, queue)
-        var exit = false
-        do {
-            exit = intCodeComputer.applyInstructionAtPosition()
-        } while (!exit)
-        return intCodeComputer.screen.countTiles(2L)
-    }
-
     @Test
     fun real() {
         val input = "/day13.txt".loadFromFile()
-        assertEquals(363, calculateDay13(input, initLongQueue()))
-
+        val longQueue = initLongQueue()
+        assertEquals(363, playArkanoid(input, longQueue).screen.countTiles(2L))
     }
 
     @Test
     fun realPart2() {
         val input = "/day13part2.txt".loadFromFile()
-        assertEquals(363, calculateDay13(input, initLongQueue()))
-
+        val winnerMoves = "/winnerMoves".loadFromFile()
+        val inputJoystick = winnerMoves.lines()[0].trim().split(",").map { it.toLong() }
+        val longQueue = initLongQueue()
+        longQueue.addAll(inputJoystick)
+        assertEquals(17159, playArkanoid(input, longQueue).screen.currentScore)
     }
 }
