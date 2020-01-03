66ce2aec899888bfd590986d0b72ae15b5473aa0
ffaure <frederic.faure@ext.cdiscount.com>
2020-01-03 18:40:02 +0100

    début day 18

--- src/main/kotlin/org/example/TritonMaze.kt
+++ src/main/kotlin/org/example/TritonMaze.kt
@@ -1,13 +1,47 @@
 package org.example
 
-class TritonMaze(input: String) {
-    fun findAccessibleKeys(): Set<Char> {
-        return findAccessiblePositions().map { maze[it]!! }.filter { it.isLowerCase() }.toSet()
+import java.util.*
+
+fun buildTritonMaze(input: String): TritonMaze {
+    val maze = mutableMapOf<Position, Char>()
+    var y = 0
+    input.split("\n").forEach {
+        for (x in 0 until it.length) {
+            val value = it[x]
+            val position = Position(x, y)
+            maze.put(position, value)
+        }
+        y++
+    }
+    return TritonMaze(maze)
+}
+
+public class TritonMaze constructor (val maze: Map<Position, Char>) {
+
+
+    val keys = mutableMapOf<Char, Position>()
+    val doors = mutableMapOf<Char, Position>()
+    var currentPosition: Position
+
+    init {
+        keys.putAll(maze.entries.filter { it.value.isLowerCase() }.associate { (k,v)-> v to k })
+        doors.putAll(maze.entries.filter { it.value.isUpperCase() }.associate { (k,v)-> v to k })
+        currentPosition = maze.filter { it.value == '@' }.keys.first()!!
     }
 
+    fun findAccessibleKeys(): Set<Char> {
+        return maze.entries.filter{ it.value.isLowerCase() }.map { it.value }.toSet()
+    }
+    fun findKeyPosition(c : Char): Position {
+        return maze.entries.find { it.value == c }!!.key
+    }
     fun findAccessiblePositions(): Set<Position> {
         val alreadyVisited = mutableSetOf<Position>()
         return accessiblePositions(currentPosition, alreadyVisited)
+    }
+
+    fun hasDoor(findAccessibleKey: Char): Boolean {
+        return maze.values.contains(findAccessibleKey.toUpperCase())
     }
 
     fun accessiblePositions(position: Position, alreadyVisited: MutableSet<Position>): MutableSet<Position> {
@@ -34,24 +68,59 @@ class TritonMaze(input: String) {
         return neighbours
     }
 
-    val maze = mutableMapOf<Position, Char>()
-    val keys = mutableMapOf<Char, Position>()
-    val doors = mutableMapOf<Char, Position>()
-    var currentPosition: Position
-
-    init {
-        var y = 0
-        input.split("\n").forEach {
-            for (x in 0 until it.length) {
-                val value = it[x]
-                val position = Position(x, y)
-                maze.put(position, value)
-                if (value.isLowerCase()) keys.put(value, position)
-                else if (value.isUpperCase()) doors.put(value, position)
-            }
-            y++
-        }
-        currentPosition = maze.filter { it.value == '@' }.keys.first()!!
+    public fun openDoor(char : Char) : TritonMaze {
+        val newMaze = maze.toMutableMap()
+        val position = findKeyPosition(char)
+        newMaze[findKeyPosition('@')] = '.'
+        newMaze[position] = '@'
+        newMaze[findKeyPosition(char.toUpperCase())] = '.'
+        return TritonMaze(newMaze)
     }
+
+    fun findShortestPath(src : Position, dest : Position): Int {
+        val visited = mutableMapOf<Position, Boolean>()
+        val q = LinkedList<QueueNode>()
+        val s = QueueNode(src, 0)
+        q.add(s)
+
+        while(q.isNotEmpty()) {
+            val curr = q.peek()
+            val pos = curr.position
+            if(pos == dest) {
+                return curr.distance
+            }
+
+            q.remove()
+            for (neighbour in getNeighbours(pos)) {
+                if(maze[neighbour] != null) {
+                    if ((maze[neighbour] == '.' || maze[neighbour]!!.isLowerCase())
+                        && visited[neighbour] == null
+                    ) {
+                        visited[neighbour] = true
+                        q.add(QueueNode(neighbour, curr.distance + 1))
+                    }
+                }
+            }
+        }
+        return -1
+    }
+
+    override fun toString(): String {
+        val builder = StringBuilder()
+        val maxx = maze.map { it.key.x }.max()!!
+        val maxy = maze.map { it.key.y }.max()!!
+        for(y in 0 until maxy) {
+            for(x in 0 until maxx) {
+                builder.append(maze[Position(x, y)])
+            }
+            builder.append('\n')
+        }
+        return builder.toString()
+    }
+
+
+
 }
+
+data class QueueNode(val position : Position, val distance : Int)
 
--- src/test/kotlin/org/example/AdventOfCodeDay18Test.kt
+++ src/test/kotlin/org/example/AdventOfCodeDay18Test.kt
@@ -10,25 +10,40 @@ class AdventOfCodeDay18Test {
     @Test
     fun sample() {
         val input = """
-            #########
-            #b.A.@.a#
-            #########
+########################
+#f.D.E.e.C.b.A.@.a.B.c.#
+######################.#
+#d.....................#
+########################
         """.trimIndent()
-        val tritonMaze = TritonMaze(input)
-
-        assertEquals(2, tritonMaze.keys.size)
-        assertEquals(1, tritonMaze.doors.size)
-        assertEquals(27, tritonMaze.maze.size)
-        assertEquals(3, tritonMaze.findAccessiblePositions().size)
-        assertEquals(1, tritonMaze.findAccessibleKeys().size)
+        val tritonMaze = buildTritonMaze(input)
+        computeDistance(tritonMaze, 0L)
     }
 
     @Test
     fun real() {
         val input = "/day18.txt".loadFromFile()
-        val tritonMaze = TritonMaze(input)
+        val tritonMaze = buildTritonMaze(input)
+        computeDistance(tritonMaze, 0L)
+    }
 
-        assertEquals(1580, tritonMaze.findAccessiblePositions().size)
-        assertEquals(10, tritonMaze.findAccessibleKeys().size)
+    val pathes = mutableSetOf<Long>()
+    fun computeDistance(maze: TritonMaze, acc : Long) {
+        val findAccessibleKeys = maze.findAccessibleKeys()
+        for (findAccessibleKey in findAccessibleKeys) {
+            val shortestPath = maze.findShortestPath(maze.currentPosition, maze.findKeyPosition(findAccessibleKey))
+            if(findAccessibleKeys.size <= 1) {
+                pathes.add(acc+shortestPath)
+            }
+            if(shortestPath>=0) {
+                println(findAccessibleKey+" "+ shortestPath)
+                if(maze.hasDoor(findAccessibleKey)) {
+                    val openDoor = maze.openDoor(findAccessibleKey)
+                    computeDistance(openDoor, acc + shortestPath)
+                }
+            }
+        }
+        println("chemin min:"+pathes.min())
+
     }
 }
