583507e6f16f953279f65fd46df9703a58aea4c4
ffaure <frederic.faure@ext.cdiscount.com>
2020-02-12 18:12:42 +0100

    day22 part 1

--- src/test/kotlin/org/example/AdventOfCodeDay22Test.kt
+++ src/test/kotlin/org/example/AdventOfCodeDay22Test.kt
@@ -48,6 +48,19 @@ class AdventOfCodeDay22Test {
     }
 
     @Test
+    fun applyCutTechniqueWithNegativeN() {
+        // ARRANGE
+        val deck = Deck(10)
+
+        // ACT
+        val result = deck.applyTechnique(Technique.CUT_N_CARDS, -4)
+
+        //
+        val expected = listOf(6, 7, 8, 9, 0, 1, 2, 3, 4, 5)
+        assertEquals(expected, result.cards)
+    }
+
+    @Test
     fun applyCutTechniqueDealWithIncrementDeck() {
         // ARRANGE
         val deck = Deck(10)
@@ -59,6 +72,98 @@ class AdventOfCodeDay22Test {
         val expected = listOf(0, 7, 4, 1, 8, 5, 2, 9, 6, 3)
         assertEquals(expected, result.cards)
     }
+
+    @Test
+    fun functionalCase1() {
+        // ARRANGE
+        val deck = Deck(10)
+
+        // ACT
+        val result =
+            deck.applyTechnique(Technique.DEAL_WITH_INCREMENT, 7)
+                .applyTechnique(Technique.DEAL_INTO_NEW_STACK)
+                .applyTechnique(Technique.DEAL_INTO_NEW_STACK)
+
+        //
+        val expected = listOf(0, 3, 6, 9, 2, 5, 8, 1, 4, 7)
+        assertEquals(expected, result.cards)
+    }
+
+    @Test
+    fun functionalCase2() {
+        // ARRANGE
+        val deck = Deck(10)
+
+        // ACT
+        val result =
+            deck
+                .applyTechnique(Technique.CUT_N_CARDS, 6)
+                .applyTechnique(Technique.DEAL_WITH_INCREMENT, 7)
+                .applyTechnique(Technique.DEAL_INTO_NEW_STACK)
+
+        //
+        val expected = listOf(3, 0, 7, 4, 1, 8, 5, 2, 9, 6)
+        assertEquals(expected, result.cards)
+    }
+
+    @Test
+    fun functionalCase3() {
+        // ARRANGE
+        val deck = Deck(10)
+
+        // ACT
+        val result =
+            deck
+                .applyTechnique(Technique.DEAL_WITH_INCREMENT, 7)
+                .applyTechnique(Technique.DEAL_WITH_INCREMENT, 9)
+                .applyTechnique(Technique.CUT_N_CARDS, -2)
+
+        //
+        val expected = listOf(6, 3, 0, 7, 4, 1, 8, 5, 2, 9)
+        assertEquals(expected, result.cards)
+    }
+
+    @Test
+    fun functionalCase4() {
+        // ARRANGE
+        val deck = Deck(10)
+
+        // ACT
+        val result =
+            deck
+                .applyTechnique(Technique.DEAL_INTO_NEW_STACK)
+                .applyTechnique(Technique.CUT_N_CARDS, -2)
+                .applyTechnique(Technique.DEAL_WITH_INCREMENT, 7)
+                .applyTechnique(Technique.CUT_N_CARDS, 8)
+                .applyTechnique(Technique.CUT_N_CARDS, -4)
+                .applyTechnique(Technique.DEAL_WITH_INCREMENT, 7)
+                .applyTechnique(Technique.CUT_N_CARDS, 3)
+                .applyTechnique(Technique.DEAL_WITH_INCREMENT, 9)
+                .applyTechnique(Technique.DEAL_WITH_INCREMENT, 3)
+                .applyTechnique(Technique.CUT_N_CARDS, -1)
+
+        //
+        val expected = listOf(9, 2, 5, 8, 1, 4, 7, 0, 3, 6)
+        assertEquals(expected, result.cards)
+    }
+
+    @Test
+    fun real() {
+        val input = "/day22.txt".loadFromFile().lines()
+        var deck = Deck(10007)
+        for(line in input) {
+            val technique = line.split(" ")
+            if(technique[0] == "cut") {
+                deck = deck.applyTechnique(Technique.CUT_N_CARDS, technique[1].toInt())
+            } else if(technique[1] == "with") {
+                deck = deck.applyTechnique(Technique.DEAL_WITH_INCREMENT, technique.last().toInt())
+            } else {
+                deck = deck.applyTechnique(Technique.DEAL_INTO_NEW_STACK)
+            }
+        }
+        assertEquals(6696, deck.cards.indexOf(2019))
+    }
+
 
 }
 
@@ -87,13 +192,21 @@ enum class Technique {
     },
     CUT_N_CARDS {
         override fun shuffle(cards: List<Int>, cardPosition: Int): List<Int> {
-            return cards.subList(cardPosition, cards.size) + cards.subList(0, cardPosition)
+            if(cardPosition >= 0) {
+                return cards.subList(cardPosition, cards.size) + cards.subList(0, cardPosition)
+            } else {
+                return cards.subList(cards.size+cardPosition, cards.size) + cards.subList(0, cards.size+cardPosition)
+            }
         }
     },
     DEAL_WITH_INCREMENT {
         override fun shuffle(cards: List<Int>, cardPosition: Int): List<Int> {
-            val positions = cards.map { if (it == 0) 0 else (it * cardPosition) % cards.size }.toList()
-            return positions
+            val positions = (0 until cards.size).map { if (it == 0) 0 else (it * cardPosition) % cards.size }.toList()
+            val arr = IntArray(positions.size)
+            for(item in positions.indices) {
+                arr[positions[item]] = cards[item]
+            }
+            return arr.toList()
         }
     };
 
